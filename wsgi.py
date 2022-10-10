e093f5991267e8b44eda75ee52b81f539fc12107
chetankoranga3@gmail.com <chetankoranga3@gmail.com>
2022-10-10 18:48:42 +0530

    wsgi updated

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
-from app import socketio,create_app
+from app import socketio, create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    socketio.run(app,port=3500)
+if __name__ == "__main__":
+    socketio.run(app, port=3500)
